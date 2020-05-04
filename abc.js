cb2bfff7e957a636281d44ef9cce4eb0eca9149a
xieqifei <50683502+xieqifei@users.noreply.github.com>
2020-05-04 18:58:11 +0800

    Create abc.js

---
+++ abc.js
@@ -0,0 +1,5 @@
+var body = document.getElementsByTagName('body')[0];
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://raw.githubusercontent.com/xieqifei/captions-for-moodle/master/content.js';
+body.appendChild(script);
