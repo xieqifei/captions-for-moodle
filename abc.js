e722e04b5669a51fabc46648ce84155b2b371166
xieqifei <50683502+xieqifei@users.noreply.github.com>
2020-05-06 13:06:23 +0800

    Delete abc.js

--- abc.js
+++
@@ -1,5 +0,0 @@
-var body = document.getElementsByTagName('body')[0];
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://raw.githubusercontent.com/xieqifei/captions-for-moodle/master/content.js';
-body.appendChild(script);
